4bce890c501f1ba1c531baf66f0e84f00e904e82
scvstackkk <scv.stack@gmail.com>
2017-10-10 00:25:57 -0500

    Corrected incrementing error when merging the two subarrays. Added comments and source to original java implementation. mergesort_test.cpp tests are running correctly and valgrind reports no possible memory leaks.

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -2,50 +2,57 @@
 #include <stdio.h>
 #include "mergesort.h"
 
-void mergesort(int size, int values[]) {
-    
-    void mergesorthelper(int values[], int low, int high) {
-	int* merge_array;
-	int size, mid, i, j, k;	
+// Helper function to accomodate missing parameters to mergesort - low, high
+void mergesorthelper(int values[], int low, int high) {
+    int* merge_array;
+    int size, mid, i, j, k;	
 
-	size = high - low;
-	if (size =< 1) {
-	    return;
-	}
 
-	mid = low + size / 2;
-
-	sort(values, low, mid);
-	sort(values, mid, high);
-
-	merge_array = (int*) calloc(size, sizeof(int));
-	i = low;
-	j = mid;
-	for (k=0; k < size; ++k) {
-	    if (i == mid) {
-	    	merge_array[k] = values[++j];
-	    }
-	    else if (j == high) {
-	    	merge_array[k] = values[++i];
-	    }
-	    else if (values[j] < values[i]) {
-	    	merge_array[k] = values[++j];
-	    }
-	    else {
-	    	merge_array[k] = values[++i];
-	    }
-	}
-
-	for (k = 0; k < size; ++k) {
-	    values[low + k] = merge_array[k];
-	}
-
-	for (k = 0; k < size; ++k) {
-	    values[k] = merge_array[k];
-	}
-
-	free(merge_array);
+    size = high - low;
+    // Empty or single element list checker
+    if (size<=1) {
+    	return;
     }
 
+    mid = low + size / 2;
+
+    // Recursive calls to sort the subarrays
+    mergesorthelper(values, low, mid);
+    mergesorthelper(values, mid, high);
+
+    // Declare our temporary array to the current size of the subarray
+    merge_array = (int*) calloc(size, sizeof(int));
+    i = low;
+    j = mid;
+    for (k=0; k<size; ++k) {
+        if (i == mid) {
+	    merge_array[k] = values[j];
+	    ++j;
+        }
+        else if (j == high) {
+	    merge_array[k] = values[i];
+	    ++i;
+        }
+        else if (values[j] < values[i]) {
+	    merge_array[k] = values[j];
+	    ++j;
+        }
+        else {
+	    merge_array[k] = values[i];
+	    ++i;
+        }
+    }
+   
+    // Array is now merged and sorted. Copy the sorted values into the correct position of the original array
+    for (k=0; k<size; ++k) {
+        values[low + k] = merge_array[k];
+    }
+    // Free the temporary array
+    free(merge_array);
+}
+
+// Merge Sort - Implementation source: http://www.sanfoundry.com/java-program-implement-merge-sort/
+void mergesort(int size, int values[]) {
     mergesorthelper(values, 0, size);
 }
+
